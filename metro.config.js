3619d1586e5e373fd6a4ce92c7e738f43a36a259
Parkjieun <0224h@naver.com>
2024-10-22 00:16:09 +0900

    initialize

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,19 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    // node_modules 디렉토리를 감시에서 제외합니다.
+    //blacklistRE: /node_modules\/.*/,
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
